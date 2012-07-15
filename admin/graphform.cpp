68555de1da4f3c70c7f300f69a022665eaf254ff
Vadim Nikitushkin <saintangel@bk.ru>
2012-07-15 13:51:17 +0400

    Update master

--- admin/graphform.cpp
+++ admin/graphform.cpp
@@ -9,9 +9,29 @@ GraphForm::GraphForm(
     ui(new Ui::GraphForm)
 {
     ui->setupUi(this);
+   // network->sendLevelTwo(clientId, MOD_GRAPH, "test");
+
+    network_->activateMode(clientId, MOD_GRAPH);
+    connect(network_, SIGNAL(dataIncome()),
+                this, SLOT(onDataReceived()));
+
+    ui->graphicsView->setScene(&scene);
 }
 
 GraphForm::~GraphForm()
 {
     delete ui;
+}
+
+void GraphForm::onDataReceived()
+{
+    if (!(network_->receivedData().from == clientId_ &&
+            network_->receivedData().mode == MOD_GRAPH))
+        return;
+    qDebug() << "GraphForm::onDataReceived()";
+    bytePicIn.clear();
+    bytePicIn = network_->receivedData().data;
+    picIn.loadFromData(bytePicIn);
+    scene.clear();
+    scene.addPixmap(picIn);
 }
