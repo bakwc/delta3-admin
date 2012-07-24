ea2107a87d80a2e571634f62c0a992cdda62154d
Dakuenjery <dakuenejry@gmail.com>
2012-07-24 14:20:39 +0300

    Create dynamic lib. Desktop UI move to new directory

---
+++ DesktopUI/main.cpp
@@ -0,0 +1,17 @@
+#include <QApplication>
+#include "UI/mainwindow.h"
+#include "network.h"
+#include "Protocols/telnet.h"
+#include "UI/telnetform.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+	delta3::Network network;//(QHostAddress("193.169.33.254"));
+
+	MainWindow w(&network);
+    w.show();
+    
+    return a.exec();
+}
